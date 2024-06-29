54525818b7cf17d0613a8a7317edca6eca4294a9
Renymara Hanna Macedo <renymara.hanna@gmail.com>
2024-06-29 12:27:22 -0300

    Adicionado função principal

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-def load_data()
+def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
-df = load_data()
+def main():
+    df = load_data()
+    st.dataframe(df)
 
-st.dataframe(df)
+if __name__ == '__main__':
+    main()
